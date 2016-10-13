5558243adff412a9c0ad17fe22c855d7050eae3c
Wellington Chagas <wellingtonpchagas@gmail.com>
2016-10-13 20:14:49 -0300

    mudanca mensagem

--- src/main/java/org/wpc/teste/loja/ClienteServlet.java
+++ src/main/java/org/wpc/teste/loja/ClienteServlet.java
@@ -15,7 +15,7 @@ public class ClienteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("CRazyy: ").append(request.getContextPath());
+		response.getWriter().append("welmau: ").append(request.getContextPath());
 	}
 
 }
