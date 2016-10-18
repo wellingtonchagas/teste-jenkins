26d00059a04f1a89e3f554bdb8aa7d0a268b8a4e
Wellington Chagas <wellingtonpchagas@gmail.com>
2016-10-17 22:38:18 -0200

    teste

--- src/main/java/org/wpc/teste/loja/ClienteServlet.java
+++ src/main/java/org/wpc/teste/loja/ClienteServlet.java
@@ -11,11 +11,11 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class ClienteServlet
  */
 @WebServlet("/ClienteServlet")
-public class ClienteServlet extends HttpServletas {
+public class ClienteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("bad: ").append(request.getContextPath());
+		response.getWriter().append("tafstafst: ").append(request.getContextPath());
 	}
 
 }
